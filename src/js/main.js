218e9657c20bab9be667a04f22bc9264f095d183
Boskolife <94291571+Boskolife@users.noreply.github.com>
2024-05-26 14:04:31 +0300

    some fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 ScrollTrigger.create({
     trigger: ".scrollTitle",
-    toggleClass: "active_scroll-title",
     start: "top top+=50px",
     endTrigger: ".text_img",
-    end: "top top+=250px",
+    end: "top top+=22%",
     pin: true,
-    pinSpacing: false,
+    // pinSpacing: false,
     scrub: 1,
   });
